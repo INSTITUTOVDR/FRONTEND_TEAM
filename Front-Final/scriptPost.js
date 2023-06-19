ed800b0d7abc9556d69cf8f9f0555056457254e2
PauloBertino <bertinopaulo99@gmail.com>
2023-06-19 20:19:30 -0300

    Post

--- Front-Final/scriptPost.js
+++ Front-Final/scriptPost.js
@@ -1,77 +1,87 @@
-let btnEnviar = document.getElementById('post')
-let nombre = document.getElementById('nombre');
-let apellido = document.getElementById('apellido');
-let email = document.getElementById('email');
-let descripcion = document.getElementById('descripcion');
+function PostFromForm() {
+  var url = "https://c453-190-120-115-22.sa.ngrok.io/api/Entities/AddModifyEntities";
 
-btnEnviar.addEventListener('click', (e) => {
+  let id = document.getElementById("id_Entitie").value;
+  let name = document.getElementById("nombre").value;
+  let surname = document.getElementById("apellido").value;
+  let email = document.getElementById("email").value;
+  let description = document.getElementById("descripcion").value;
 
-    e.preventDefault()
+  // Validar los campos requeridos
+  if (
+    id === "" ||
+    name === "" ||
+    surname === "" ||
+    email === "" ||
+    description === ""
+  ) {
+    swal({
+      title: "Atención",
+      html: "Faltan datos",
+      type: "error",
+      backdrop: true,
+      allowOutsideClick: false,
+      showCancelButton: false,
+      showConfirmButton: true,
+      cancelButtonColor: "#DD6B55",
+      confirmButtonColor: "#DD6B55",
+    });
+  } else {
+    // Crear un objeto FormData y agregar los campos y valores
+    var formData = new FormData();
+    formData.append("id_Entitie", id);
+    formData.append("name", name);
+    formData.append("surname", surname);
+    formData.append("email", email);
+    formData.append("description", description);
 
-    const formData = new FormData();      
-   
-    formData.append('name', nombre.value);
-    formData.append('surname', apellido.value);
-    formData.append('email', email.value);
-    formData.append('description', descripcion.value);
+    // Configurar las opciones de la solicitud
+    var requestOptions = {
+      method: "POST",
+      body: formData,
+    };
 
+    fetch(url, requestOptions)
+      .then((response) => response.text())
+      .then((data) => {
+        var json = JSON.parse(data);
+        console.log(json);
+        console.log(json.status);
+        if (json.status == "201") {
+          swal({
+            title: "Atención",
+            html: "Datos agregados con éxito",
+            type: "success",
+            backdrop: true,
+            allowOutsideClick: false,
+            showCancelButton: false,
+            showConfirmButton: true,
+            cancelButtonColor: "#DD6B55",
+            confirmButtonColor: "#DD6B55",
+          });
+          // Limpiar los campos
+          document.getElementById("id_Entitie").value = "";
+          document.getElementById("nombre").value = "";
+          document.getElementById("apellido").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("descripcion").value = "";
+        } else {
+          swal({
+            title: "Atención",
+            html: "Algo salió mal",
+            type: "error",
+            backdrop: true,
+            allowOutsideClick: false,
+            showCancelButton: false,
+            showConfirmButton: true,
+            cancelButtonColor: "#DD6B55",
+            confirmButtonColor: "#DD6B55",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }
+}
 
-    if (nombre.value === '' || apellido.value === '' || email.value === '' || descripcion.value === '') {
-        swal({
-            title: 'Atencion',
-                html: 'Faltan datos',
-                type: 'error',
-                backdrop: true,
-                allowOutsideClick: false,
-                showCancelButton: false,
-                showConfirmButton: true,
-                cancelButtonColor: '#DD6B55',
-                confirmButtonColor: '#DD6B55'
-            })
-    } else {
-
-        fetch('', {
-        method: 'POST',
-        body: formData
-        }).then(response => response.text())
-        .then(data => {
-            json = JSON.parse(data)
-            console.log(json);
-            console.log(json.status);
-            if(json.status == '200') {
-                swal({
-                    title: 'Atencion',
-                    html: 'Datos Agregados con exito',
-                    type: 'success',
-                    backdrop: true,
-                    allowOutsideClick: false,
-                    showCancelButton: false,
-                    showConfirmButton: true,
-                    cancelButtonColor: '#DD6B55',
-                    confirmButtonColor: '#DD6B55'
-                })
-                nombre.value = '';
-                apellido.value = '';
-                email.value = '';
-                descripcion.value = '';
-            } else {
-                swal({
-                    title: 'Atencion',
-                    html: 'Algo Salio Mal',
-                    type: 'error',
-                    backdrop: true,
-                    allowOutsideClick: false,
-                    showCancelButton: false,
-                    showConfirmButton: true,
-                    cancelButtonColor: '#DD6B55',
-                    confirmButtonColor: '#DD6B55'
-                })
-            }
-            
-        })
-        .catch(error => {
-            console.log('Error');
-        })
-
-    }
-})
